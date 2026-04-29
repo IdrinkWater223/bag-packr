1e15db47ea7196ec52c4d82636e1adcf468354b6
IdrinkWater <rjaheim614@gmail.com>
2026-04-28 20:55:29 -0500

    Add timetable notification feature

---
+++ main.py
@@ -0,0 +1,28 @@
+from datetime import datetime as dt
+import requests
+
+
+timetable = {
+    "Monday":    ["Mathematics", "Metal Work", "History", "Physical Education"],
+    "Tuesday":   ["Spanish", "English Language", "Personal Development", "Biology", "Visual Arts"],
+    "Wednesday": ["Chemistry", "Biology", "Geography", "Physics", "Building & Technology"],
+    "Thursday":  ["Technical Drawing", "English Language", "Social Studies", "Mathematics"],
+    "Friday":    ["Physics", "Mathematics", "Music", "English Literature", "Information Technology"],
+}
+
+day = dt.now().strftime("%A")
+send = requests.post
+
+def notis():
+    notification = str(timetable[day]).strip("[]").replace("'", "")
+    send("https://ntfy.sh/Time_Table", 
+        data=notification.encode(encoding="utf-8"), 
+        headers={
+            "Title": "📚 Your Time Table for Today",
+            "Priority": "urgent"})
+
+if day in timetable:
+    notis()
+else:
+    print("No school today")
+
